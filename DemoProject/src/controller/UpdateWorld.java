2920346992d1a0f2cbee473715d5829f71a1ff10
Hoang Anh Cyber <Hoang Anh Cyber@DESKTOP-78RDCGP>
2020-04-28 17:01:33 +0700

    test update world from controller

---
+++ DemoProject/src/controller/UpdateWorld.java
@@ -0,0 +1,20 @@
+package controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import org.json.JSONException;
+
+import dao.WorldDAO;
+
+public class UpdateWorld {
+	public static void main(String args[]) {
+		WorldDAO wD = new WorldDAO();
+		try {
+			wD.updateWorldStatistics();
+		} catch (JSONException | IOException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
