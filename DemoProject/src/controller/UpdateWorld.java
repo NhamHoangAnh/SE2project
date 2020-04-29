2911ba9daa8fca30454113dc0bc2621c2d6c719c
Hoang Anh Cyber <Hoang Anh Cyber@DESKTOP-78RDCGP>
2020-04-29 19:00:38 +0700

    World statistics update daily

--- DemoProject/src/controller/UpdateWorld.java
+++ DemoProject/src/controller/UpdateWorld.java
@@ -3,12 +3,19 @@ package controller;
 import java.io.IOException;
 import java.sql.SQLException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.json.JSONException;
 
 import dao.WorldDAO;
 
-public class UpdateWorld {
-	public static void main(String args[]) {
+public class UpdateWorld extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 		WorldDAO wD = new WorldDAO();
 		try {
 			wD.updateWorldStatistics();
