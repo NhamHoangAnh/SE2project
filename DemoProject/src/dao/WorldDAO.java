2d344b63f0544c70c025f940514fd15d73ec5a5a
Hoang Anh Cyber <Hoang Anh Cyber@DESKTOP-78RDCGP>
2020-04-29 18:59:26 +0700

    update WD get world statistics day by day

--- DemoProject/src/dao/WorldDAO.java
+++ DemoProject/src/dao/WorldDAO.java
@@ -3,6 +3,7 @@ package dao;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
@@ -23,33 +24,41 @@ public class WorldDAO {
 		JSONObject world = Fetch.fetchWorldStatistics();
 		String globalDate = Fetch.fetchDate();
 		
+		PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM WORLD ORDER BY wId DESC LIMIT 1");
+		ResultSet r = pstmt.executeQuery();
+		r.next();
+		String dateBefore = r.getString("date");
+		int wId = r.getInt("wId");
 		
-		String updateWorld = " UPDATE World SET " + 
-				"newConfirmed = ?" +
-				", totalConfirmed = ?" +
-				", newDeaths = ?" +
-				", totalDeaths = ?" +
-				", newRecovered = ?" +
-				", totalRecovered =?" +
-				", date = ?" +
-				"WHERE wId = ?;";	
-		double newConfirmed = world.getDouble("NewConfirmed");
-		double totalConfirmed = world.getDouble("TotalConfirmed");
-		double newDeaths = world.getDouble("NewDeaths");
-		double totalDeaths = world.getDouble("TotalDeaths");
-		double newRecovered = world.getDouble("NewRecovered");
-		double totalRecovered = world.getDouble("TotalRecovered");
-		String date = globalDate;
-		PreparedStatement preparedStatement = conn.prepareStatement(updateWorld);
-		preparedStatement.setDouble(1, newConfirmed);
-		preparedStatement.setDouble(2, totalConfirmed);
-		preparedStatement.setDouble(3, newDeaths);
-		preparedStatement.setDouble(4, totalDeaths);
-		preparedStatement.setDouble(5, newRecovered);
-		preparedStatement.setDouble(6, totalRecovered);
-		preparedStatement.setString(7, date);
-		preparedStatement.setInt(8, 1);
-		preparedStatement.executeUpdate();
+		if (!dateBefore.equals(globalDate)) {
+			wId = wId + 1;
+			String updateWorld = " UPDATE World SET " + 
+					"newConfirmed = ?" +
+					", totalConfirmed = ?" +
+					", newDeaths = ?" +
+					", totalDeaths = ?" +
+					", newRecovered = ?" +
+					", totalRecovered =?" +
+					", date = ?" +
+					"WHERE wId = ?;";	
+			double newConfirmed = world.getDouble("NewConfirmed");
+			double totalConfirmed = world.getDouble("TotalConfirmed");
+			double newDeaths = world.getDouble("NewDeaths");
+			double totalDeaths = world.getDouble("TotalDeaths");
+			double newRecovered = world.getDouble("NewRecovered");
+			double totalRecovered = world.getDouble("TotalRecovered");
+			String date = globalDate;
+			PreparedStatement preparedStatement = conn.prepareStatement(updateWorld);
+			preparedStatement.setDouble(1, newConfirmed);
+			preparedStatement.setDouble(2, totalConfirmed);
+			preparedStatement.setDouble(3, newDeaths);
+			preparedStatement.setDouble(4, totalDeaths);
+			preparedStatement.setDouble(5, newRecovered);
+			preparedStatement.setDouble(6, totalRecovered);
+			preparedStatement.setString(7, date);
+			preparedStatement.setInt(8, wId);
+			preparedStatement.executeUpdate();
+		}
 	}
 	
 	public World selectWorldStatistics() throws SQLException, JSONException, IOException{
@@ -77,11 +86,18 @@ public class WorldDAO {
 		return w;	
 	}
 	
-	public static void main(String args[]) {
+	public static void main(String args[]) throws SQLException, IOException {
 		try {
 			String globalDate = Fetch.fetchDate();
 			System.out.println(globalDate);
-		} catch (JSONException | IOException | SQLException e) {
+//			Connection connect = DBConnect.getConnection();
+//			PreparedStatement ps = connect.prepareStatement("SELECT * FROM World ORDER BY wId DESC LIMIT 1");
+//			ResultSet r = ps.executeQuery();
+//			while (r.next()) {
+//				System.out.println(r.getString("newConfirmed"));
+//			}
+			
+		} catch (JSONException | SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
