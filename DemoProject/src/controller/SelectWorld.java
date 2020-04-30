ceac3c3e862e9aca5607c9b582647df724d72e8b
Hoang Anh Cyber <Hoang Anh Cyber@DESKTOP-78RDCGP>
2020-04-30 23:39:10 +0700

    Select World on server

--- DemoProject/src/controller/SelectWorld.java
+++ DemoProject/src/controller/SelectWorld.java
@@ -1,27 +1,57 @@
 package controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONException;
 
 import com.google.gson.Gson;
 import dao.WorldDAO;
 import model.World;
 
+@WebServlet("/index")
 public class SelectWorld extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private Gson gson = new Gson();
+	private WorldDAO wD = new WorldDAO();
+	private World world = new World();
+	
+	public SelectWorld() {
+		super();
+	}
 	
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
-		res.setContentType("application/json");
+		res.setContentType("text/html");
         res.setCharacterEncoding("UTF-8");
         
+        try {
+			world = wD.selectWorldStatistics();
+		} catch (JSONException | SQLException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         
-        WorldDAO wD = new WorldDAO();
-        World w = new World();
+        // world as JSON
+        
+//        String worldJSON = this.gson.toJson(world);
+//        PrintWriter pw = res.getWriter();
+//        pw.print(world);
+//        pw.flush();
+        
+        req.setAttribute("world", world);
+        
+        RequestDispatcher dispatcher = req.getRequestDispatcher("index.jsp");
+        dispatcher.include(req, res);
         
 	}
+	
+	
 }
