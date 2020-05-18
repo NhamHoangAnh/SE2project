e8686dec53b323fe4b5bf5353dc0ceb62312d043
Hoang Anh Cyber <Hoang Anh Cyber@DESKTOP-78RDCGP>
2020-05-19 00:05:38 +0700

    update userController

--- DemoProject/src/controller/UserController.java
+++ DemoProject/src/controller/UserController.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import dao.UserDAO;
-import controller.SelectAllCountries;
+import controller.CountriesController;
 
 @WebServlet("/index")
 public class UserController extends HttpServlet{
